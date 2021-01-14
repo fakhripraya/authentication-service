d11aaf9a3e2c4d9dde4021a96c8ff40b97d9faa3
fakhripraya <fakhri@indomaret.co.id>
2021-01-15 02:09:30 +0700

    Update db_master_role_access.go

--- database/db_master_role_access.go
+++ database/db_master_role_access.go
@@ -4,8 +4,9 @@ import "time"
 
 // MasterRoleAccess is an entity that directly communicate with the MasterRoleAccess table in the database
 type MasterRoleAccess struct {
-	AccessID   uint      `gorm:"primary_key;not null" json:"id"`
-	RoleID     uint      `gorm:"primary_key;not null" json:"role_id"`
+	ID         uint      `gorm:"primary_key;autoIncrement;not null" json:"id"`
+	AccessID   uint      `gorm:"not null" json:"access_id"`
+	RoleID     uint      `gorm:"not null" json:"role_id"`
 	Created    time.Time `gorm:"type:datetime" json:"created"`
 	CreatedBy  string    `json:"created_by"`
 	Modified   time.Time `gorm:"type:datetime" json:"modified"`
