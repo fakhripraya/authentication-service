cc3c62aaebb296920a126c84b16b38b3779f4ddf
fakhripraya <fakhri@indomaret.co.id>
2021-06-15 18:05:59 +0700

    Update main.go

--- main.go
+++ main.go
@@ -70,8 +70,7 @@ func init() {
 	}
 
 	googleOauthConfig = &oauth2.Config{
-		// RedirectURL:  "http://" + appConfig.API.Host + ":" + strconv.Itoa(appConfig.API.Port) + "/google/callback",
-		RedirectURL:  "http://localhost:8080/google/callback",
+		RedirectURL:  "http://" + appConfig.API.Host + ":" + strconv.Itoa(appConfig.API.Port) + "/google/callback",
 		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
 		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
 		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"},
